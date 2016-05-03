f51ac5536fe5c72541b129df31e3ba02e17cf271
LCY Vincent <lauchunyin@gmail.com>
2016-05-03 23:25:17 +0800

    Update 2016-05-03T15:25:05.400Z

--- functions/analyze/common/pages.js
+++
@@ -1,5 +0,0 @@
-module.exports ={
-  'hk':{"hk.nextmedia":"105259197447","southchinamorningpost":"355665009819","mingpaoinews":"498203090239831","hongkongeconomicjournal":"139543009419663"},
-  'tw':{"appledaily.tw":"232633627068","news.ebc":"124616330906800","m.ltn.tw":"394896373929368","TheNewsLens":"481065478629613","myudn":"241284961029",
-  "ETtoday":"242305665805605","ctitv.news":"283285647874"}
-};
--- test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-var index = require('./index');
-var getUrlByEndpoint = index.getUrlByEndpoint;
-var fetchLatestPostIds = index.fetchLatestPostIds;
-var _ = require('lodash');
-
-
-
-// client.hset("hash key", "hashtest 1", "some value", redis.print);
