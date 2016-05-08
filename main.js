b90c6ceffb80eba21ab504be1f28296ec369498a
vincentlaucy <lauchunyin@gmail.com>
2016-05-08 14:36:27 +0800

    Update 2016-05-08T06:36:22.739Z

--- main.js
+++ main.js
@@ -106,6 +106,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	(0, _reactIntl.addLocaleData)([].concat(_toConsumableArray(_zh2.default)));
 
 
+	// won't auto update until gulp script
+	var REACTION_TYPES = __webpack_require__(343).REACTION_TYPES;
+
 	var FETCH_AGG = 'FETCH_AGG';
 	function getEndpoint(location) {
 	  var url = 'https://8zbfsx31e0.execute-api.ap-northeast-1.amazonaws.com/prod/reactions';
@@ -116,7 +119,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	// TODO by daily (HK) emotion?
 	// http://stackoverflow.com/questions/260857/changing-website-favicon-dynamically
 	(function () {
-	  var reaction = _lodash2.default.sample(['haha', 'angry', 'love', 'wow', 'sad']);
+	  var reaction = _lodash2.default.sample(REACTION_TYPES);
 	  var link = document.createElement('link');
 	  link.type = 'image/x-icon';
 	  link.rel = 'shortcut icon';
@@ -69363,6 +69366,31 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	// exports
 
+
+/***/ },
+/* 343 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	//TODO refactor and combine as 1 obj
+
+	var REACTION_TYPES = ['LIKE', 'LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY', 'THANKFUL'];
+
+	var pages = {
+	  'hk': { "hk.nextmedia": "105259197447", "southchinamorningpost": "355665009819", "mingpaoinews": "498203090239831", "hongkongeconomicjournal": "139543009419663" },
+	  'tw': { "appledaily.tw": "232633627068", "news.ebc": "124616330906800", "m.ltn.tw": "394896373929368", "TheNewsLens": "481065478629613", "myudn": "241284961029",
+	    "ETtoday": "242305665805605", "ctitv.news": "283285647874" }
+	};
+
+	var pageNamesById = { "105259197447": "蘋果日報", "355665009819": "South China Morning Post SCMP", "200954406608272": "香港獨立媒體網", "420361564693683": "PassionTimes 熱血時報", "498203090239831": "明報即時新聞", "139543009419663": "信報財經新聞 hkej.com", "113816764690": "香港經濟日報 hket.com", "126994627339633": "am730", "448747665308662": "香港01", "148108245239100": "頭條日報", "569337929781203": "852郵報", "180447291099": "Metro Daily Hong Kong《都市日報》",
+	  "232633627068": "蘋果日報", "124616330906800": "東森新聞", "242305665805605": "ETtoday新聞雲", "394896373929368": "自由時報", "283285647874": "快點報報", "481065478629613": "The News Lens 關鍵評論網", "241284961029": "udn.com 聯合新聞網" };
+
+	module.exports = {
+	  REACTION_TYPES: REACTION_TYPES,
+	  pages: pages,
+	  pageNamesById: pageNamesById
+	};
 
 /***/ }
 /******/ ])
